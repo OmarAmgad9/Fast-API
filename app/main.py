4f4fc9ff0a189191654789b6299ea1016a82e856
Omar Amgad <103358321+OmarAmgad9@users.noreply.github.com>
2024-04-20 02:16:55 +0200

    CRUD with ORM or SQLALCHEMY

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status,Depends, HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
 import time
+from . import models
+from .database import engine, SessionLocal, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI()
 
 class Post(BaseModel):
-    name: str
+    title: str
     content: str
 
 while True:
@@ -48,10 +55,10 @@ def get_latest_post():
         "message": "GET latest post",
         "data": post
     }
-@app.post("/post")
+@app.post("/post", status_code=status.HTTP_201_CREATED)
 def create_post(post:Post):
 
-    cursor.execute(""" INSERT INTO posts (name, content) VALUES (%s, %s) RETURNING * """, (post.name, post.content))
+    cursor.execute(""" INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING * """, (post.title, post.content))
     post = cursor.fetchone()
     conn.commit()
     return {
@@ -60,7 +67,7 @@ def create_post(post:Post):
     }
 @app.put("/post/{id}")
 def update_post(post:Post, id:str):
-    cursor.execute("""UPDATE posts SET name=%s, content=%s WHERE id=%s RETURNING *;""", (post.name, post.content, str(id)))
+    cursor.execute("""UPDATE posts SET title=%s, content=%s WHERE id=%s RETURNING *;""", (post.title, post.content, str(id)))
     post = cursor.fetchone()
     conn.commit()
     return {
@@ -77,4 +84,65 @@ def delete_post(id:str):
     return {
         "message": f"Delete post with {id} was successfully",
         "data": post,
+    }
+
+# CRUD operation with ORM 
+
+
+@app.get("/posts/orm")
+def posts_ORM(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {
+        "message": "GET All The posts",
+        "data": posts,
+    }
+@app.get("/posts/orm/{id}")
+def post_ORM_id(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id==id).first()
+    return {
+        "message": "Get specific post with id",
+        "data": post,
+    }
+
+@app.post("/posts/orm")
+def create_post(post:Post, db:Session=Depends(get_db)):
+    # you can replace this with
+    new_post =  models.Post(**post.dict())
+    # new_post = models.Post(title=post.title, content=post.content)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {
+        "message": "Post Created",
+        "data": new_post,
+    }
+
+@app.delete("/posts/orm/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post_orm(id:int, db:Session=Depends(get_db)):
+    delete_post = db.query(models.Post).filter(models.Post.id==id).first()
+    db.delete(delete_post)
+    db.commit()
+    
+    return {
+        "message": "Delete this post is successfully",
+        "data": delete_post,
+    }
+@app.put("/posts/orm/{id}")
+def update_post_orm(id:int,update_post:Post ,db:Session=Depends(get_db)):
+
+    post_query = db.query(models.Post).filter(models.Post.id==id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post With id:{id} Does not exist")
+    # post_query.update(post.dict(),synchronize_session=False)
+    # post_query.title = post.title
+    # post_query.content = post.content
+    # db.add(post_query)
+    # post_query.update({'title': post.title, 'content': post.content}, synchronize_session=False)
+    post_query.update(update_post.dict(), synchronize_session=False)
+    db.commit()
+    return {
+        "message": "Update this post is successfully",
+        "data": post_query.first(),
     }
